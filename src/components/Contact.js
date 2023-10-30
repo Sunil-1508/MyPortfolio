e429829e749ca3cf65532992758e6fd5d5ca15bc
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-10-30 13:16:24 +0530

    final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
             <div className="row pb-5 px-4">
               <div className="col-md-4 col-sm-6">
                 <ul className="nav flex-column">
-                 <li className="nav-item mb-2"> <a href="callto:+917013370983" className="nav-link nounderline p-0"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="CurrentColor" className="bi bi-telephone-fill" viewBox="0 0 16 16">
+                 <li className="nav-item mb-2"> <a href="tel:+917013370983" className="nav-link nounderline p-0"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="CurrentColor" className="bi bi-telephone-fill" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                 </svg> +91 7013370983</a></li>
                 <li className="nav-item mb-2"><a href="mailto:20a91a05b1@aec.edu.in" className="nav-link nounderline p-0"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope-fill" viewBox="0 0 16 16">
@@ -67,8 +67,7 @@ export default function Contact() {
                     </div>
                     <button
                       className="btn btn-primary"
-                      type="button"
-                      fdprocessedid="j4swj7">Send</button>
+                      type="button">Send</button>
                   </div>
                 </form>
               </div>
@@ -80,7 +79,7 @@ export default function Contact() {
               </p>
               <button
                       className="btn btn-primary" 
-                      type="button"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="gray" className="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+                      type="button"><a href="/"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="gray" className="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
                       </svg></a></button>
              
